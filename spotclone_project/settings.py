2672abef423b900e40f148d31dd75d855e60f081
Ivanov Ivan <ivan.di.ivanov@a1.bg>
2023-08-04 16:21:22 +0300

    Added additional css

--- spotclone_project/settings.py
+++ spotclone_project/settings.py
@@ -160,10 +160,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = reverse_lazy('user_login')
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL')
-EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
